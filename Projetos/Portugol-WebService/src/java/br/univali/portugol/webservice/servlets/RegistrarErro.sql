44d19fbb2babc20a336125c22246c00228c96a2b
Fillipi Pelz <fillipi.pelz@gmail.com>
2012-05-07 21:05:42 -0300

    retirando fontes de outros repos

--- Projetos/Portugol-WebService/src/java/br/univali/portugol/webservice/servlets/RegistrarErro.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO erro 
-(
-    aplicativo,	versao, mensagem, classe, stacktrace, 
-	
-    algoritmo, data, informacoesSO, informacoesJVM, macAddress
-)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
